fc1352d2cbdaef69497b9279c31655da18c6f364
MT2Dev <61083853+MT2Dev@users.noreply.github.com>
2023-03-13 20:35:47 +0300

    ENG Lang.

--- AStyle/AStyle - MT2Dev Version/Client SRC/CWebBrowser/AStyle.bat
+++ AStyle/AStyle - MT2Dev Version/Client SRC/CWebBrowser/AStyle.bat
@@ -1,7 +1,26 @@
 @echo off
 astyle -A1 -T -C -S -N -xW -xw -xg -xd -H -U -y -xb -j -xy -n -v CWebBrowser.c CWebBrowser.h
 
-REM                             HANGI FLAG NE ISE YARAR ?                             (Hazirlayan; MT2Dev - 2023)
+REM    ENG LANG                    ALL FLAGS (with info)                             (MT2Dev - 2023)
+REM   -A1 = Change coding style with Eric Allman's coding style "Allman Style" (Info; https://en.wikipedia.org/wiki/Indentation_style#Allman_style)
+REM   -T  = Make all spaces to TAB in fonctions.
+REM   -C  = Edits all spaces in classes. (like Allman Style)
+REM   -S  = Edits all spaces in switch's. (like Allman Style)
+REM   -N  = Edits all spaces in NameSpace's. (like Allman Style)
+REM   -xW = Edits all spaces in long ifdef/endif blocks. (like Allman Style)
+REM   -xw = Aligns ifdef sections with codes.
+REM   -xg = Leaves a space after the comma.
+REM   -xd = It leaves a space before the first parenthesis at the end of the code.
+REM   -H  = It leaves a space between the headers and the code start.
+REM   -U  = Deletes unnecessary spaces after or before the end of the parenthesis.
+REM   -y  = Moves the else sections within the condition to a new line.
+REM   -xb = Headers used on the same line are expanded and moved to a lower line.
+REM   -j  = If there is no after header, it adds braces. (Braces = {})
+REM   -xy = Changes "> >" in the template to ">>". (ATTENTION!: C++11 or higher is required for this operation!)
+REM   -n  = It ensures that the backups of the edited files are not taken, you can remove this flag if you want to have a backup.
+REM   -v  = Debug mode. (for extra logs)
+REM    /***********************************************************************************************************/
+REM    TR LANG                    HANGI FLAG NE ISE YARAR ?                             (Hazirlayan; MT2Dev - 2023)
 REM   -A1 = Eric Allman'in kodlama stili olan "Allman Style" (Bilgi; https://en.wikipedia.org/wiki/Indentation_style#Allman_style)
 REM   -T  = Fonsiyon icerisinde gereken tum bosluklari TAB kullanarak ayarlar.
 REM   -C  = Classlar icerisindeki bosluklari ayarlar. (Allman stiline uygun sekilde.)
